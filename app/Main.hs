4ae44780b4c046c9012eab2924f518bc8c3557dc
Michael Mackus <michaelmackus@gmail.com>
2020-08-11 07:20:32 -0700

    Show risk map

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import SDL
-import SDL.Image (loadTexture)
-import SDL.Vect (V4(..))
+import Graphics.Rect
+import Graphics.Pixels
+
 import Control.Monad (unless)
+import SDL
+import SDL.Image (load)
+import SDL.Vect (V4(..))
 
 main :: IO ()
 main = do
   initializeAll
-  window <- createWindow "My SDL Application" defaultWindow
+  HintRenderScaleQuality $= ScaleLinear
+  let windowConfig = defaultWindow { windowResizable = True }
+  window   <- createWindow "My SDL Application" windowConfig
   renderer <- createRenderer window (-1) defaultRenderer
-  tex <- loadTexture renderer "res/image/Risk_game_map_fixed.png"
-  appLoop renderer tex
+  {-- load our image and obtain the raw pixel data --}
+  surface  <- load "res/image/Risk_game_map_fixed.png"
+  pixels   <- surfaceToColors surface
+  print . take 100 . dropWhile (== 0) $ pixels
+  {-- create the SDL texture and pass to game loop --}
+  texture  <- createTextureFromSurface renderer surface
+  appLoop window renderer texture
 
-appLoop :: Renderer -> Texture -> IO ()
-appLoop renderer tex = do
+bgColor = V4 200 200 200 200
+
+appLoop :: Window -> Renderer -> Texture -> IO ()
+appLoop window renderer texture = do
   events <- pollEvents
   let eventIsQPress event =
         case eventPayload event of
           KeyboardEvent keyboardEvent ->
             keyboardEventKeyMotion keyboardEvent == Pressed &&
             keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          WindowClosedEvent _ -> True
           _ -> False
       qPressed = any eventIsQPress events
   clear renderer
-  copy renderer tex Nothing Nothing
+  rendererDrawColor renderer $= bgColor
+  maxSize <- get (windowSize window)
+  texInfo <- queryTexture texture
+  target  <- mkRectangleWithin (textureDimensions texInfo) maxSize
+  copy renderer texture Nothing (Just target)
   present renderer
-  unless qPressed (appLoop renderer tex)
+  unless qPressed (appLoop window renderer texture)
---
+++ src/Graphics/Rect.hs
@@ -0,0 +1,33 @@
+module Graphics.Rect
+    ( mkRectangleWithin
+     ,textureDimensions
+    ) where
+
+import SDL
+import SDL.Vect (V4(..), V2(..), Point(..))
+import Foreign.C.Types
+
+mkRectangleWithin :: V2 CInt -> V2 CInt -> IO (Rectangle CInt)
+mkRectangleWithin (V2 w h) (V2 outerw outerh) =
+    let (w', h') = clampDimensionsTo (w, h) (outerw, outerh)
+        (x,  y ) = translateOrigin (w, h) (outerw, outerh)
+    in  return $ Rectangle (P (V2 x y)) (V2 w' h')
+
+textureDimensions :: TextureInfo -> V2 CInt
+textureDimensions (TextureInfo _ _ w h) = V2 w h
+
+-- translate origin so that the rect is in the center of outerw x outerh
+translateOrigin :: Integral a => (a, a) -> (a, a) -> (a, a)
+translateOrigin (w, h) (outerw, outerh) = (centerx, centery)
+    where
+        centerx = round $ max 0 ((fromIntegral outerw - fromIntegral w) / 2)
+        centery = round $ max 0 ((fromIntegral outerh - fromIntegral h) / 2)
+
+-- ensure dimensions are clamped to maxw x maxh
+clampDimensionsTo :: Integral a => (a, a) -> (a, a) -> (a, a)
+clampDimensionsTo (w, h) (maxw, maxh)
+        | w < maxw && h < maxh = (w, h)
+        | w > h     = (maxw, round ((fromIntegral maxw)/aspect))
+        | otherwise = (round ((fromIntegral maxh)*aspect), maxh)
+    where
+        aspect = fromIntegral w / fromIntegral h
