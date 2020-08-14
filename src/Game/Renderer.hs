37a3c9fe6943effc51acbb79c7c24e61cf874f0d
Michael Mackus <michaelmackus@gmail.com>
2020-08-13 20:53:03 -0700

    Add simple animated loading text

--- src/Game/Renderer.hs
+++ src/Game/Renderer.hs
@@ -3,25 +3,30 @@ module Game.Renderer (renderLoadingScreen, updateRenderer) where
 
 import Game.Types
 
+import Control.Concurrent (threadDelay)
 import Control.Monad.Reader
 import Foreign.C.Types
 import Graphics.Rect
 import SDL
+import qualified Data.Text as T
 import qualified SDL.Font as Font
 import qualified Control.Monad.State as State
 
 bgColor = V4 200 200 200 200
 
 renderLoadingScreen :: Renderer -> Font.Font -> IO ()
-renderLoadingScreen r f = do
-  clear r
-  rendererDrawColor r $= bgColor
-  s       <- Font.solid f (V4 0 0 0 255) "Loading..."
-  fontT   <- createTextureFromSurface r s
-  texInfo <- queryTexture fontT
-  target  <- mkRectangleWithin (textureDimensions texInfo) (V2 1227 600)
-  copy r fontT Nothing (Just target)
-  present r
+renderLoadingScreen r font = f "Loading"
+  where f text = do
+          clear r
+          rendererDrawColor r $= bgColor
+          s       <- Font.solid font (V4 0 0 0 255) text
+          fontT   <- createTextureFromSurface r s
+          texInfo <- queryTexture fontT
+          target  <- mkRectangleWithin (textureDimensions texInfo) (V2 1227 600)
+          copy r fontT Nothing (Just target)
+          present r
+          threadDelay 300000
+          unless (text == "Loading...") $ f (T.append text ".")
 
 updateRenderer :: GameRenderer ()
 updateRenderer = do
